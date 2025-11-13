50b588213f66606619200867b8cbfe467b61b04f
nux-thephox <ajsmtz12@hotmail.com>
2025-11-12 21:41:38 -0600

    Proyecto Base MS-Transaccion
    Este Ms, almacenara una transacción muy basica de venta en una BD h2

---
+++ CuartaSemana_MicroServicios/Examen/MS-Transaccion/src/main/java/com/mx/transaccion/MsTransaccionApplication.java
@@ -0,0 +1,13 @@
+package com.mx.transaccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransaccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransaccionApplication.class, args);
+	}
+
+}
