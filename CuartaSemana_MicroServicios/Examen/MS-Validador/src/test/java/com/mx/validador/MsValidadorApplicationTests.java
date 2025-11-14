3a9ab5b51c6e3947fd92c836fac8d0389f2c25bd
nux-thephox <ajsmtz12@hotmail.com>
2025-11-14 02:57:41 -0600

    Proyecto Base del microservicio de validacion
    Este ms validara la peticion de transacción. De igual manera solo sera una transaccion muy basica de venta.

---
+++ CuartaSemana_MicroServicios/Examen/MS-Validador/src/test/java/com/mx/validador/MsValidadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.validador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsValidadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
